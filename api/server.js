16ad6ceb268c3790cfb6a5874afc02e487574264
jacqlynb <jacqlyn.bender@gmail.com>
2021-09-11 20:24:55 -0700

    create basic node app

---
+++ api/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const port = 8080;
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+// add image
+app.post('/image', (req, res) => {
+  //TODO
+});
+
+// search for image
+app.get('/image', (req, res) => {
+  //TODO
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
+
+module.exports = {
+  app,
+};
