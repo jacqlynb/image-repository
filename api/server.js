9e334f2ab43aab6c25d6c84bf22a456de95b1b8f
jacqlynb <jacqlyn.bender@gmail.com>
2021-09-12 20:35:56 -0700

    implement endpoints for showing/creating images

--- api/server.js
+++ api/server.js
@@ -3,9 +3,6 @@ const cors = require('cors');
 const mysql = require('mysql');
 const port = 8080;
 
-const app = express();
-app.use(cors());
-
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -14,15 +11,31 @@ const connection = mysql.createConnection({
   charset: 'utf8',
 });
 
+const app = express();
+app.use(express.json());
+app.use(cors());
+
 // add image
 app.post('/image', (req, res) => {
-  //TODO
+  const { url, title, tags } = req.body;
+  connection.query(
+    `INSERT INTO images (image_url, image_title, image_labels) VALUES("${url}", "${title}", "${tags}")`,
+    (error, results) => {
+      if (error) {
+        res.status(500);
+        res.json();
+      } else {
+        res.status(201);
+        res.json();
+      }
+    }
+  );
 });
 
 // search for image
 app.get('/image', (req, res) => {
   connection.query('SELECT * FROM images', (error, results) => {
-    error ? res.send(error) : res.send(results[0]);
+    error ? res.send(error) : res.send(results);
   });
 });
 
